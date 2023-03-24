5aa2c823b5a5d566330073bd2987841427df49ba
JasonCaoAU <jason2019au@gmail.com>
2023-03-25 00:46:31 +1100

    finished

---
+++ README.md
@@ -0,0 +1,15 @@
+### Write Countdown Application
+
+### Technologies used:
+
+Python, Visual Studio Code, Git
+
+### Project Description:
+
+1-Write an application that accepts a user input of a goal and a deadline (date). Print the remaining time until that deadline.
+
+### Usage:
+
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime
+
+user_input = input("Please enter your goal with a deadline separated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+deadline_date_formatted = datetime.datetime.strptime(deadline,"%d.%m.%Y")
+
+today_date = datetime.datetime.today()
+
+time_till = (deadline_date_formatted - today_date).days
+print(f"Dear user! Time remaining for your goal: {goal} is {time_till} days" )
